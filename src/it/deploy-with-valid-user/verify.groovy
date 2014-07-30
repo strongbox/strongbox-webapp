aa06019e452fcf49d7eee0ecea48e249d39241f8
Martin Todorov <carlspring@gmail.com>
2014-07-30 17:09:46 +0100

    #SB-141: Make the strongbox-webapp include all the resources from the strongbox-resources modules
    Sorted out and brought back into a buildable state.

--- src/it/deploy-with-valid-user/verify.groovy
+++ src/it/deploy-with-valid-user/verify.groovy
@@ -1,18 +1,11 @@
-import java.io.*;
-import java.util.*;
+import org.carlspring.maven.commons.util.ArtifactUtils
+import org.carlspring.strongbox.client.ArtifactClient
 
-try
-{
-    // System.out.println("Checking whether all the artifacts with duplicate versions are correctly resolved...");
-    // TODO: Perform a proper check by checking the length of the remote file
-    
-    return true;
-}
-catch( Throwable t )
-{
-    System.out.println(" *[ Check failed ]* ");
-    t.printStackTrace();
-    return false;
-}
 
-return true;
+def artifact = ArtifactUtils.getArtifactFromGAV("org.carlspring.maven:test-project:1.0.6");
+
+def client = new ArtifactClient();
+client.setUsername("maven");
+client.setPassword("password");
+
+return client.artifactExists(artifact, "storage0", "releases");
