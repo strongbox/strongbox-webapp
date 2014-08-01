1412d315a0e2f0cea0dfcf53c690b05efd173e55
Martin Todorov <carlspring@gmail.com>
2014-08-01 18:46:27 +0100

    #SB-233: Move all the maven-invoker-plugin tests into the strongbox-webapp
    Implemented.

--- src/it/deploy-with-invalid-user-in-memory/verify.groovy
+++ src/it/deploy-with-invalid-user-in-memory/verify.groovy
@@ -7,6 +7,6 @@ def artifact = ArtifactUtils.getArtifactFromGAV("org.carlspring.maven:test-proje
 def client = new ArtifactClient();
 
 // This should throw a ResponseException as the user is not valid.
-def response = client.artifactExistsStatusCode(artifact, "storage0", "snapshots");
+def response = client.artifactExistsStatusCode(artifact, "storage0", "snapshots-in-memory");
 
 return response.getStatus() == 401;
